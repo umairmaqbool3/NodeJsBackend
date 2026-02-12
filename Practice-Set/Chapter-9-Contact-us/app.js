7a7aee776dae978dd80fc163466500e8e9f7cb15
umair <umair.maqbool55@gmail.com>
2026-02-12 19:43:42 +0500

    Chapter 9 practice completed

--- Practice-Set/Chapter-9-Contact-us/app.js
+++ Practice-Set/Chapter-9-Contact-us/app.js
@@ -9,7 +9,29 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
     console.log('Second middleware : ', req.method, req.url);
-    res.send('Hello from Express!');
+    next();
+});
+
+app.get('/',(req, res, next) => {
+  console.log('Handling / path--------', req.method, req.url);
+  res.send('Hello from Express!');
+});
+
+app.get('/contact-us', (req, res, next) => {
+  console.log('Handling /contact path--------', req.method, req.url);
+  res.send(`
+    <h1>Please give your details here</h1>
+    <form action="/contact-us" method="POST">
+      <input type="text" name="name" placeholder="Your Name" required />
+      <input type="email" name="email" placeholder="Your Email" required />
+      <button type="submit">Submit</button>
+    </form>
+  `);
+});
+
+app.post('/contact-us', (req, res, next) => {
+  console.log('Handling POST / contact path--------', req.method, req.url);
+  res.send(`<h1>Thank you for contacting us!</h1>`);
 });
 
 const PORT = 3000;
