16c3faa89d73d2512cf4242db42c9fe953edab6c
umair <umair.maqbool55@gmail.com>
2026-02-22 21:15:37 +0500

    Intro to mongodb

---
+++ Chapter-16-Intro-To-Mongodb/controllers/hostController.js
@@ -0,0 +1,68 @@
+const Home = require("../models/home");
+
+exports.getAddHome = (req, res, next) => {
+  res.render("host/edit-home", {
+    pageTitle: "Add Home to airbnb",
+    currentPage: "addHome",
+    editing: false,
+  });
+};
+
+exports.getEditHome = (req, res, next) => {
+  const homeId = req.params.homeId;
+  const editing = req.query.editing === 'true';
+
+  Home.findById(homeId).then(([homes]) => {
+    const home = homes[0];
+    if (!home) {
+      console.log("Home not found for editing.");
+      return res.redirect("/host/host-home-list");
+    }
+
+    console.log(homeId, editing, home);
+    res.render("host/edit-home", {
+      home: home,
+      pageTitle: "Edit your Home",
+      currentPage: "host-homes",
+      editing: editing,
+    });
+  });
+};
+
+exports.getHostHomes = (req, res, next) => {
+  Home.fetchAll().then(([registeredHomes]) => {
+    res.render("host/host-home-list", {
+      registeredHomes: registeredHomes,
+      pageTitle: "Host Homes List",
+      currentPage: "host-homes",
+    })
+  });
+};
+
+exports.postAddHome = (req, res, next) => {
+  const { houseName, price, location, rating, imageUrl,description } = req.body;
+  const home = new Home(houseName, price, location, rating, imageUrl,description);
+  home.save().then(() => {
+    console.log("Home added successfully.");
+  });
+
+  res.redirect("/host/host-home-list");
+};
+
+exports.postEditHome = (req, res, next) => {
+  const { id, houseName, price, location, rating, imageUrl, description } = req.body;
+  const home = new Home(houseName, price, location, rating, imageUrl, description, id);
+  home.save(false);
+  res.redirect("/host/host-home-list");
+};
+
+exports.postDeleteHome = (req, res, next) => {
+  const homeId = req.params.homeId;
+  console.log('Came to delete ', homeId);
+  Home.deleteById(homeId).then(() => {
+    res.redirect("/host/host-home-list");
+  }).catch(err => {
+    console.log("Error deleting home: ", err);
+    // res.redirect("/host/host-home-list");
+  });
+};
---
+++ Chapter-16-Intro-To-Mongodb/models/home.js
@@ -0,0 +1,33 @@
+
+const {getDb} = require("../utils/database");
+
+module.exports = class Home {
+  constructor(houseName, price, location, rating, imageUrl,description, id) {
+    this.houseName = houseName;
+    this.price = price;
+    this.location = location;
+    this.rating = rating;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.id = id;
+  }
+
+  save() {
+   const db = getDb();
+   console.log("Saving home to database:", db);
+   return db.collection("homes").insertOne(this);
+  // console.log("Saving home to database:", this);
+  }
+
+  static fetchAll() {
+   return db.execute("SELECT * FROM homes");
+  }
+
+  static findById(homeId) {
+    return db.execute("SELECT * FROM homes WHERE id = ?", [homeId]);
+  }
+
+  static deleteById(homeId) {
+    return db.execute("DELETE FROM homes WHERE id = ?", [homeId]);
+  }
+};
