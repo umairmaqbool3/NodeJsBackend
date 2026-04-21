76c1a3434df5338d37d57fa13c7a4e958e3e08d0
umair <umair.maqbool55@gmail.com>
2026-04-21 12:03:18 +0500

    nest js with graphql started

---
+++ nest-js-graphql/src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { BookModule } from './book/book.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({isGlobal:true}),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+      playground: true
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI!),
+    BookModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
